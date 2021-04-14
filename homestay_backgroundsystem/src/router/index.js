b8afd476074b917a2d877193d9e048d46def9ce0
VocalWang <67056424+VocalWang@users.noreply.github.com>
2021-04-15 00:40:30 +0800

    4.15-0:40

--- homestay_backgroundsystem/src/router/index.js
+++ homestay_backgroundsystem/src/router/index.js
@@ -5,10 +5,12 @@ Vue.use(VueRouter)
 
 import Login from "../views/Login";
 import Main from "../views/main/Main";
+import List from "../views/main/List";
 
 let routes = [
     {path: "/", component: Main},
-    {path: "/login", component: Login}
+    {path: "/login", component: Login},
+    {path: "/list",component: List}
 ]
 let router = new VueRouter({
     routes
--- homestay_backgroundsystem/src/views/Login.vue
+++ homestay_backgroundsystem/src/views/Login.vue
@@ -1,15 +1,83 @@
 <template>
-    <div>
-        login
+    <div class="login">
+        <div class="login-content">
+            <el-form ref="loginFrom" :model="loginFrom" :rules="rules">
+                <el-form-item label="用户名：" prop="username">
+                    <el-input v-model="loginFrom.username">
+
+                    </el-input>
+                </el-form-item>
+                <el-form-item label="密 码：" prop="password">
+                    <el-input v-model="loginFrom.password">
+
+                    </el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click.prevent="handleLogin">登录</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Login"
+        name: "Login",
+        data() {
+            return {
+                loginFrom: {
+                    username: "",
+                    password: ""
+                },
+                rules: {
+                    username: [
+                        {required: true, message: "请输入用户名", trigger: "blur"},
+                        {min: 5, max: 10, message: "长度在5到10个字符", trigger: "blur"}
+                    ],
+                    password: [
+                        {required: true, message: "请输入密码", trigger: "blur"},
+                        {min: 5, max: 10, message: "长度在5到10个字符", trigger: "blur"}
+                    ]
+                }
+            }
+        },
+        methods: {
+            handleLogin() {
+                this.$refs.loginFrom.validate(valid => {
+                    // console.log(valid);
+                    if (valid){
+                        console.log(this.loginFrom);
+                        this.$router.push({path:"/"})
+                    }
+                })
+
+            }
+        }
     }
 </script>
 
 <style scoped>
+    .login {
+        position: absolute;
+        left: 0;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        /*background: #f5f5f5;*/
+        background-image: url("https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fac-q.static.booking.cn%2Fimages%2Fhotel%2Fmax1024x768%2F120%2F120912261.jpg&refer=http%3A%2F%2Fac-q.static.booking.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1621002716&t=83197c66081dfa70caeef546a3884674");
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
 
+    .login > .login-content {
+        width: 400px;
+        height: auto;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        border-radius: 5px;
+        background: #fff;
+        padding: 20px;
+    }
 </style>
